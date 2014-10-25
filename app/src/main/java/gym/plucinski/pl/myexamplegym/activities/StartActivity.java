6d69d6d33c570ddc107f1821c49aa4b7d7acdca1
krzysztofplucinski <kris19850811@gazeta.pl>
2014-10-25 09:15:31 +0200

    Initial commit

---
+++ app/src/main/java/gym/plucinski/pl/myexamplegym/activities/StartActivity.java
@@ -0,0 +1,15 @@
+package gym.plucinski.pl.myexamplegym.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import gym.plucinski.pl.myexamplegym.R;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
